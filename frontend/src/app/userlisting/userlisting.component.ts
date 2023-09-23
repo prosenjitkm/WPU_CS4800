e92503b458509a4c5c63de37e739e98d32674cbf
Prosenjit Kumar Mandal <prosenjit.mandal.1991@gmail.com>
2023-09-23 17:50:25 -0400

    added update pop up

--- frontend/src/app/userlisting/userlisting.component.ts
+++ frontend/src/app/userlisting/userlisting.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { AuthService } from "../service/auth.service";
 import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatDialog} from "@angular/material/dialog";
+import {UpdatepopupComponent} from "../updatepopup/updatepopup.component";
+
 
 @Component({
   selector: 'app-userlisting',
@@ -17,26 +22,41 @@ export class UserlistingComponent {
     'isActive',
     'action'];
 
-  constructor(private service:AuthService){
+  constructor(private service:AuthService, private dialog:MatDialog){
     this.loadUser();
 
   }
 
   userlist:any;
   dataSource:any;
+  @ViewChild(MatPaginator) paginator !:MatPaginator;
+  @ViewChild(MatSort) sort !:MatSort;
 
   loadUser(){
     this.service.getAll().subscribe(response =>{
       this.userlist=response;
       this.dataSource=new MatTableDataSource(this.userlist);
+      this.dataSource.paginator=this.paginator;
+      this.dataSource.sort=this.sort;
     });
   }
 
   updateUser(code:any){
-
+    this.dialog.open(UpdatepopupComponent, {
+      enterAnimationDuration: '1000ms',
+      exitAnimationDuration: '500ms',
+      width: '50%',
+      data: {
+        usercode:code
+      }
+    });
   }
 
   deleteUser(code:any){
+
+  }
+
+  openDialog(){
 
   }
 }
