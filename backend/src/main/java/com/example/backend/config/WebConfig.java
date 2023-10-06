6d56d509bb5aabcd5ece87ced1187ef9b6c11148
prosenjit <prosenjit.mandal.1991@gmail.com>
2023-10-05 23:51:04 -0400

    working on front end

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -7,11 +7,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+/*    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // All endpoints in the application
                 .allowedOrigins("http://localhost:4200") // Source origin allowed to access the end points
                 .allowedMethods("*") // Allowed methods (GET, POST, PUT, etc.)
                 .maxAge(3600); // Cache duration for CORS preflight responses.
+    }*/
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:63342")
+                .allowedMethods("*")
+                .maxAge(3600);
     }
 }
