f8eb34b0fe84b1a2b9660f9a3f4eda5d3b7d0e55
Michael Fuller <michael09fuller@gmail.com>
2023-11-09 15:02:39 -0500

    branch worked 11/08
    Signed-off-by: Michael Fuller <michael09fuller@gmail.com>

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,28 +6,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule}  from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { RegisterComponent } from './components/authentication/register/register.component';
-import { LoginComponent } from './components/authentication/login/login.component';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { PageNotFoundComponent} from './pagenotfound/pagenotfound.component';
 
+import { HeaderComponent } from "./header/header.component";
+import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { PageNotFoundComponent } from './pagenotfound/pagenotfound.component';
 // ... other imports
 
 @NgModule({
   declarations: [
     AppComponent,
-
     // ... your components here
+
     HeaderComponent,
-    RegisterComponent,
     LoginComponent,
-    HomeComponent,
+    RegisterComponent,
     DashboardComponent,
+    HomeComponent,
     ProductListComponent,
     PageNotFoundComponent
+
   ],
   imports: [
     BrowserModule,
@@ -35,10 +36,11 @@ import { PageNotFoundComponent} from './pagenotfound/pagenotfound.component';
     HttpClientModule,
     FormsModule,
     BrowserModule,
-
+    AppRoutingModule,
     // ... other modules here
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+aa
