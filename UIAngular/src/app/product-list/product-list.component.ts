63e28fcd5fab5f693c82cc6e27b6160c301315d8
prosenjit <prosenjit.mandal.1991@gmail.com>
2023-12-04 02:13:04 -0500

    added comments

---
+++ UIAngular/src/app/product-list/product-list.component.ts
@@ -0,0 +1,23 @@
+/*product-list.component.ts*/
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/productModel';
+import { CartService } from "../service/cart/cart.service";
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[] = [];
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    // Get your products here
+  }
+
+  onAddToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+}
